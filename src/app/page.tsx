07dbea7d306488c7980c8572afac79d72b312166
Нигматжонов Азизжон Нодиржон <aziz.nigmatjonov7@gmail.com>
2025-10-01 20:08:40 +0500

    added pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    fetch(`https://full-stack-react-app.onrender.com/api/articles`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
   return <div></div>;
 }
