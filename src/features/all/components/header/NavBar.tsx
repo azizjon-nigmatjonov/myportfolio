17f430f9864f0fc721de59dfe25be8275e2086ff
Azizjon <macbookairm2@MacBook-Air-MacBook.local>
2026-01-03 22:44:45 +0500

    fixed: header

--- src/features/all/components/header/NavBar.tsx
+++ src/features/all/components/header/NavBar.tsx
@@ -6,18 +6,18 @@ import { MyInfo } from "@/types/auth";
 export default function NavBar({ myInfo = {} as MyInfo }: { myInfo: MyInfo }) {
   return <nav>
     <ul className="flex gap-4">
-      <li className="flex items-center gap-2 group">
+      {myInfo.github_url && <li className="flex items-center gap-2 group">
         <Link target="_blank" href={myInfo.github_url || ''}>Github </Link>
         <ArrowRightIcon className="transform rotate-[-45deg] group-hover:rotate-0 transition-transform duration-300 ease-in-out" />
-      </li>
-      <li className="flex items-center gap-2 group">
+      </li>}
+      {myInfo.linkedin_url && <li className="flex items-center gap-2 group">
         <Link target="_blank" href={myInfo.linkedin_url || ''}>Linkedin </Link>
         <ArrowRightIcon className="transform rotate-[-45deg] group-hover:rotate-0 transition-transform duration-300 ease-in-out" />
-      </li>
-      <li className="flex items-center gap-2 group">
+      </li>}
+      {myInfo.resume_url && <li className="flex items-center gap-2 group">
         <Link target="_blank" href={myInfo.resume_url || ''}>Read.cv </Link>
         <ArrowRightIcon className="transform rotate-[-45deg] group-hover:rotate-0 transition-transform duration-300 ease-in-out" />
-      </li>
+      </li>}
     </ul>
   </nav>
 }
