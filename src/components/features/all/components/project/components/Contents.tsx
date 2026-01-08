db85eb84cad20570dc789b9caba9668e22838ae5
Azizjon <macbookairm2@MacBook-Air-MacBook.local>
2026-01-08 11:47:04 +0500

    changed style of text

--- src/components/features/all/components/project/components/Contents.tsx
+++ src/components/features/all/components/project/components/Contents.tsx
@@ -6,7 +6,7 @@ export default function ProblemStatement({ portfolio }: { portfolio: Portfolio }
     return <div className="container">
         <div className="py-20">
             <h3 className="pb-8 text-3xl font-semibold">Problem</h3>
-            {portfolio.problem_statement?.split('\n').filter((line) => line.trim()).map((line, index) => <p className="sm:text-[16px]" key={index}>{line.replace(/^\d+\)\s*/, '')}<br /></p>)}
+            {portfolio.problem_statement?.split('\n').filter((line) => line.trim()).map((line, index) => <p className="sm:text-[16px]" key={index}>{line.replace(/^\d+\)\s*/, '- ')}<br /></p>)}
         </div>
     </div>
 }
@@ -15,7 +15,7 @@ export function ProductionDetails({ portfolio }: { portfolio: Portfolio }) {
     return <div className="container">
         <div className="py-20 flex sm:space-x-20 space-x-0 flex-col space-y-8 sm:space-y-0 sm:flex-row">
             <h3 className="text-3xl font-semibold whitespace-nowrap">Production <br /> technology in detail</h3>
-            {(portfolio.production_detailed_statment || portfolio.description)?.split('\n').filter((line) => line.trim()).map((line, index) => <p className="sm:text-[16px]" key={index}>{line.replace(/^\d+\)\s*/, '')}<br /></p>)}
+            {(portfolio.production_detailed_statment || portfolio.description)?.split('\n').filter((line) => line.trim()).map((line, index) => <p className="sm:text-[16px]" key={index}>{line.replace(/^\d+\)\s*/, '- ')}<br /></p>)}
         </div>
     </div>
 }
